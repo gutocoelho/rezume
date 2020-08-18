b9077deada3e3bb8daa4e4986a18ac49195e7e77
gutocoelho <augustoufc@hotmail.com>
2020-08-17 23:35:01 -0300

    profissioanis

--- index.php
+++ index.php
@@ -145,7 +145,7 @@
 		<div class="container">
 			<div class="row">
 				<div class="section-heading text-center col-md-12">
-					<h2>Featured <strong>Portfolio</strong></h2>
+					<h2>Profissionais <strong>que eu sigo</strong></h2>
 				</div>
 			</div>
 			<div class="filters">
