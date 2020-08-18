f3546067ad23c633c6d4f99958c36c14cab60e66
gutocoelho <augustoufc@hotmail.com>
2020-08-17 23:01:23 -0300

    NOva index

--- index.php
+++ index.php
@@ -30,10 +30,10 @@
 			<div class="collapse navbar-collapse justify-content-md-center" id="navbarsExample09">
 				<ul class="navbar-nav">
 					<li class="nav-item"><a class="nav-link" href="#section-home">Home</a></li>
-					<li class="nav-item"><a class="nav-link" href="#section-portfolio">Portfolio</a></li>
-					<li class="nav-item"><a class="nav-link" href="#section-resume">Resume</a></li>
+					<li class="nav-item"><a class="nav-link" href="#section-portfolio">Experiências</a></li>
+					<li class="nav-item"><a class="nav-link" href="#section-resume">Quem eu sigo</a></li>
 					<li class="nav-item"><a class="nav-link" href="#section-about">About</a></li>
-					<li class="nav-item"><a class="nav-link" href="#section-contact">Contact</a></li>
+					<li class="nav-item"><a class="nav-link" href="#section-contact">Contato</a></li>
 				</ul>
 			</div>
 		</div>
