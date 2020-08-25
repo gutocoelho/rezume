9594482804dd9e6af46e636343a07f12bfe6feb3
gutocoelho <augustoufc@hotmail.com>
2020-08-24 22:16:31 -0300

    caracteres aspas duplas

--- index.php
+++ index.php
@@ -17,12 +17,12 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta property="og:locale" content="pt_BR" />
 	<meta property="og:type" content="website" />	
-	<meta property='og:title' content="Augusto Coelho"/>
-	<meta property='og:image' content="http://augusto.mygeekbox.com.br/images/augusto_coelho.jpg"/>
+	<meta property="og:title" content="Augusto Coelho"/>
+	<meta property="og:image" content="http://augusto.mygeekbox.com.br/images/augusto_coelho.jpg"/>
 	<meta property="og:image:width" content="1900" />
 	<meta property="og:image:height" content="1296" />
-	<meta property='og:description' content="Designer, Especialista em Gestão de Produtos e Mídias Digitais - Saiba Mais"/>
-	<meta property='og:url' content='http://augusto.mygeekbox.com.br'/>
+	<meta property="og:description" content="Designer, Especialista em Gestão de Produtos e Mídias Digitais - Saiba Mais"/>
+	<meta property="og:url" content="http://augusto.mygeekbox.com.br"/>
 	<link rel="stylesheet" href="css/animate.css">
 	<link rel="stylesheet" href="css/flexslider.css">
 	<link rel="stylesheet" href="fonts/icomoon/style.css">
